d85d97b2cbc060a79a3eb76d42ece1d862879ba4
Dhaval Kapil <me@dhavalkapil.com>
2017-03-02 00:36:29 +0530

    Made new_ip of unicast server to be configurable

--- receive_unicast.py
+++ receive_unicast.py
@@ -7,25 +7,23 @@ from lib import arp
 
 def usage():
     print "Usage:"
-    print "%s [interface] [new_mac] [time(seconds)]" % (sys.argv[0])
+    print "%s [interface] [new_mac] [new_ip] [time(seconds)]" % (sys.argv[0])
 
 def main():
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         usage()
         sys.exit()
 
     interface = sys.argv[1]
     new_mac = sys.argv[2]
-    seconds = int(sys.argv[3])
-
-    addresses = netifaces.ifaddresses(interface)
-    src_ip = addresses[netifaces.AF_INET][0]['addr']
+    new_ip = sys.argv[3]
+    seconds = int(sys.argv[4])
 
     def packet_handler(packet):
-        if packet[ARP].pdst == src_ip and \
+        if packet[ARP].pdst == new_ip and \
            packet[ARP].op == arp.ARP_REQUEST:
             reply_packet = arp.create_arp_packet(new_mac,
-                                                 src_ip,
+                                                 new_ip,
                                                  packet[ARP].hwsrc,
                                                  packet[ARP].psrc,
                                                  arp.ARP_REPLY)
