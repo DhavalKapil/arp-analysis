8ca7915d9d83208ba91b51ce34408f662655151d
Dhaval Kapil <me@dhavalkapil.com>
2017-04-11 08:55:10 +0000

    Added code to sniff network and log all ARP requests

---
+++ sniff_network.py
@@ -0,0 +1,48 @@
+import netifaces
+import sys
+import json
+import time
+from scapy.all import *
+
+from lib import arp
+
+def usage():
+    print "Usage:"
+    print "%s [time(seconds)]" % (sys.argv[0])
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+        sys.exit()
+
+    seconds = int(sys.argv[1])
+
+    packet_log = []
+    def packet_handler(packet):
+        print(packet)
+        packet_log.append({
+            "timestamp": int(time.time()),
+            "packet": {
+                "eth_header": {
+                    "dst": packet[Ether].dst,
+                    "src": packet[Ether].src,
+                    },
+                "arp_header": {
+                    "source_mac": packet[ARP].hwsrc,
+                    "source_ip": packet[ARP].psrc,
+                    "dest_mac": packet[ARP].hwdst,
+                    "dest_ip": packet[ARP].pdst
+                    }
+                }
+            })
+
+    arp.sniff_arp_packets(callback=packet_handler,
+                          timeout=seconds)
+
+    with open("log.json", "w") as outfile:
+        json.dump(packet_log, outfile)
+
+    print "Finished successfully"
+
+if __name__== "__main__":
+    main()
